383f0f9018833b274d0ecfa3c952cb27da6e730c
Ashesh Shrestha <asheshshrestha2@gmail.com>
2020-08-19 23:10:45 +0545

    accounts -> model -> adduser -> error while adding (not done)

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -13,18 +13,19 @@ class MyUserManager(BaseUserManager):
     def create_user(self,phone,password=None):
         if not phone:
             raise ValueError("Users must have a phone number")
-        user = self.model(phone = phone)
+        user = self.model(phone = phone,)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
     def create_superuser(self,phone,password = None):
         user = self.create_user(
             phone=phone,
             password=password
         )
-        user.is_admin =True
-        user.save()
+        user.is_admin = True
+        user.save(using=self._db)
         return user
+
 
 
 class User(AbstractBaseUser):
@@ -32,23 +33,21 @@ class User(AbstractBaseUser):
     phone = models.CharField(validators=[phone_regex],max_length=15,unique=True)
     name = models.CharField(max_length=20,blank=True,null=True)
     first_login = models.BooleanField(default=False)
-    active = models.BooleanField(default=False)
-    staff = models.BooleanField(default=False)
-    admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD  = 'phone'
     REQUIRED_FIELDS = []
     
-    objects = UserManager()
+    objects = MyUserManager()
 
     def __str__(self):
         return self.phone
-    def has_perm(self,app_label):
-        return True
     @property
     def is_admin(self):
-        return True
+        return self.admin
     @property
     def is_active(self):
         return self.active
